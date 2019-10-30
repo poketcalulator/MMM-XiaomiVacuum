6e53581825a33c687a3c89d1e462606abd388917
poketcalulator <john.kristensen@stofanet.dk>
2019-10-30 21:30:27 +0100

    John

---
+++ MMM-XiaomiVacuum.js
@@ -0,0 +1,371 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-XiaomiVacuum
+ * ver 1.0
+ * By John Kristensen
+ * MIT Licensed.
+ */
+
+Module.register('MMM-XiaomiVacuum', {
+    defaults: {
+        token: '',
+        ipAddress: '',
+        updateInterval: 5 * 1000, // 1 miniute
+        animationSpeed: 2 * 1000, // 2 seconds
+    },
+
+    requiresVersion: '2.1.0',
+
+    start() {
+        const self = this;
+
+        self.loaded = false;
+        self.stats = {};
+
+        self.sendSocketNotification('START', self.config);
+        Log.info('Starting module: ' + self.name);
+    },
+
+    getDom() {
+        const self = this;
+
+        if (self.error) {
+            return self.renderError();
+        }
+
+        if (!self.loaded) {
+            return self.renderLoading();
+        }
+
+        return self.renderStats();
+    },
+
+    renderError() {
+        const self = this;
+
+        let wrapper = document.createElement('div');
+        wrapper.className = 'dimmed light small';
+        wrapper.innerHTML = self.error;
+        return wrapper;
+    },
+
+    renderLoading() {
+        const self = this;
+
+        let wrapper = document.createElement('div');
+        wrapper.className = 'dimmed light small';
+        wrapper.innerHTML = self.translate('LOADING');
+
+        return wrapper;
+    },
+
+    renderStats() {
+        const self = this;
+
+        let wrapper = document.createElement('table');
+        wrapper.className = 'small';
+        wrapper.innerHTML = `
+			<tr>
+				${self.renderName()}
+				${self.renderPhase()}
+				${self.renderBinStatus()}
+				${self.renderBatteryStatus()}
+			</tr>
+			<tr>
+				${self.RenderFanspeed()}
+			</tr>
+			<tr>
+				${self.RenderFilterWorkTime()}
+			</tr>
+      <tr>
+				${self.RenderSideBrushWorkTime()}
+			</tr>
+			<tr>
+				${self.RendermainBrushWorkTime()}
+			</tr>
+			<tr>
+				${self.RendersensorDirtyTime()}
+			</tr>
+		`;
+
+        return wrapper;
+    },
+
+    renderName() {
+
+        var name = this.stats.name.replace(".VACUUM", "");
+
+        return `<td class="name">${name}</td>`;
+    },
+
+    renderPhase() {
+        const self = this;
+
+        let phaseText;
+        switch (self.stats.phase) {
+            case 'initiating':
+                phaseText = self.translate('INITIATING');
+                break;
+            case 'charger-offline':
+                phaseText = self.translate('CHARGER_OFFLINE');
+                break;
+            case 'waiting':
+                phaseText = self.translate('WAITING');
+                break;
+            case 'cleaning':
+                phaseText = self.translate('CLEANING');
+                break;
+            case 'returning':
+                phaseText = self.translate('RETURNING');
+                break;
+            case 'charging':
+                phaseText = self.translate('CHARGING');
+                break;
+            case 'charging-error':
+                phaseText = self.translate('CHARGING_ERROR');
+                break;
+            case 'paused':
+                phaseText = self.translate('PAUSED');
+                break;
+            case 'spot-cleaning':
+                phaseText = self.translate('SPOT_CLEANING');
+                break;
+            case 'error':
+                phaseText = self.translate('ERROR');
+                break;
+            case 'shutting-down':
+                phaseText = self.translate('SHUTTING_DOWN');
+                break;
+            case 'updating':
+                phaseText = self.translate('UPDATING');
+                break;
+            case 'docking':
+                phaseText = self.translate('DOCKING');
+                break;
+            case 'unknown-16':
+                phaseText = self.translate('GOING_TO_THE_TARGET');
+                break;
+            case 'zone-cleaning':
+                phaseText = self.translate('ZONE_CLEANING');
+                break;
+            case 'full':
+                phaseText = self.translate('FULL');
+                break;
+            default:
+                phaseText = `${self.translate('UNKNOWN')}: ${self.stats.phase}`;
+        }
+
+        return `<td class="phase title bright">${phaseText}</td>`;
+    },
+
+    RendersensorDirtyTime() {
+        const self = this;
+
+        let sensorDirtyPercent = Math.floor(100 - (this.stats.sensordirtytime) / 108000 * 100);
+        let sensorDirtyHours = Math.floor(this.stats.sensordirtytime / 3600);
+        let sensorDirtyRemain = (30 - sensorDirtyHours); //this one is for model S50, improvement here maybe ¯\_(ツ)_/¯
+
+        return `
+	<td class="name"><i class="fas fa-sliders"></i>${self.translate('SENSOR_DIRTY_TIME')}</td>
+	<td class="bin title bright">${sensorDirtyHours}${self.translate('HOURS')}</td>
+	<td class="battery">${self.translate('REMAIN')} ${sensorDirtyPercent}% / ${sensorDirtyRemain}${self.translate('HOURS')}</td>`;
+
+    },
+
+
+
+    RendermainBrushWorkTime() {
+        const self = this;
+
+        let mainBrushWTPercent = Math.floor(100 - (this.stats.mainBrush) / 1080000 * 100);
+        let mainBrushWTHours = Math.floor(this.stats.mainBrush / 3600);
+        let mainBrushHoursRemain = (300 - mainBrushWTHours); //this one is for model S50, improvement here maybe ¯\_(ツ)_/¯
+
+        return `
+	<td class="name"><i class="fas fa-sliders"></i>${self.translate('MAINBRUSH_WORK_TIME')}</td>
+	<td class="bin title bright">${mainBrushWTHours}${self.translate('HOURS')}</td>
+	<td class="battery">${self.translate('REMAIN')} ${mainBrushWTPercent}% / ${mainBrushHoursRemain}${self.translate('HOURS')}</td>`;
+
+    },
+
+
+    RenderSideBrushWorkTime() {
+        const self = this;
+
+        let sidebrushWTPercent = Math.floor(100 - (this.stats.sidebrush) / 720000 * 100);
+        let sidebrushWTHours = Math.floor(this.stats.sidebrush / 3600);
+        let sidebrushHoursRemain = (200 - sidebrushWTHours); //this one is for model S50, improvement here maybe ¯\_(ツ)_/¯
+
+        return `
+	<td class="name"><i class="fas fa-sliders"></i>${self.translate('SIDEBRUSH_WORK_TIME')}</td>
+	<td class="bin title bright">${sidebrushWTHours}${self.translate('HOURS')}</td>
+	<td class="battery">${self.translate('REMAIN')} ${sidebrushWTPercent}% / ${sidebrushHoursRemain}${self.translate('HOURS')}</td>`;
+
+    },
+
+    RenderFilterWorkTime() {
+        const self = this;
+
+        let filterWTPercent = Math.floor(100 - (this.stats.filterworktime) / 540000 * 100);
+        let filterWTHours = Math.floor(this.stats.filterworktime / 3600);
+        let filterHoursRemain = (150 - filterWTHours); //this one is for model S50, improvement here maybe ¯\_(ツ)_/¯
+
+        return `
+			<td class="name"><i class="fas fa-sliders"></i>${self.translate('FILTER_WORK_TIME')}</td>
+			<td class="bin title bright">${filterWTHours}${self.translate('HOURS')}</td>
+			<td class="battery">${self.translate('REMAIN')} ${filterWTPercent}% / ${filterHoursRemain}${self.translate('HOURS')}</td>`;
+
+    },
+
+
+
+    RenderFanspeed() {
+        const self = this;
+
+        let fanSpeedtxt;
+        switch (this.stats.fanspeed) {
+            case 0: // gen1, gen2 and gen3
+                fanSpeedtxt = self.translate('FANSPEED_IDLE');
+                break;
+            case 38: // gen1, gen2 and gen3
+                fanSpeedtxt = self.translate('FANSPEED_QUIET');
+                break;
+            case 105: // gen 2
+                fanSpeedtxt = self.translate('FANSPEED_MOPPING');
+                break;
+            case 38: // gen1, gen2 and gen3
+                fanSpeedtxt = self.translate('FANSPEED_QUIET');
+                break;
+            case 60: // gen1, gen2 and gen3
+                fanSpeedtxt = self.translate('FANSPEED_BALANCED');
+                break;
+            case 75: // gen2
+                fanSpeedtxt = self.translate('FANSPEED_TURBO');
+                break;
+            case 77: // gen1
+                fanSpeedtxt = self.translate('FANSPEED_TURBO');
+                break;
+            case 103: // gen3
+                fanSpeedtxt = self.translate('FANSPEED_TURBO');
+                break;
+            case 90: // gen1
+                fanSpeedtxt = self.translate('FANSPEED_MAX_SPEED');
+                break;
+            case 100: // gen2
+                fanSpeedtxt = self.translate('FANSPEED_MAX_SPEED');
+                break;
+            case 104: // gen3
+                fanSpeedtxt = self.translate('FANSPEED_MAX_SPEED');
+                break;
+            default: // could not identify fan speed - new model or has user hacked preset settings?
+                fanSpeedtxt = `${self.translate('UNKNOWN')}: ${this.stats.fanspeed}`;
+        }
+
+        return `
+			<td class="name"><i class="fas fa-sliders"></i>${self.translate('FANSPEED_LABLE')}</td>
+			<td class="bin title bright">${fanSpeedtxt}</td>
+			<td class="battery">${this.stats.fanspeed}</td>`;
+    },
+
+
+
+
+    renderBinStatus() {
+        const self = this;
+
+        let statusHtml = '';
+        if (self.stats.phase == "full") {
+            statusHtml = `
+				<td class="bin title bright">
+					<i class="fas fa-trash"></i> ${self.translate('FULL')}
+				</td>
+			`;
+        }
+
+        return statusHtml;
+    },
+
+
+    renderBatteryStatus() {
+        const self = this;
+
+        let bp = (this.stats.batteryPercent)
+
+        if (self.stats.phase == "charging") {
+            return `
+	<td class="battery">
+	<i class="fas fa-plug"></i> ${bp}%
+	</td>`;
+        };
+
+        if (bp >= 0 && bp < 12) {
+            return `
+	<td class="battery">
+	<i class="fas fa-battery-empty"></i> ${bp}%
+	</td>`;
+        };
+
+        if (bp >= 13 && bp < 25) {
+            return `
+	<td class="battery">
+	<i class="fas fa-battery-quarter"></i> ${bp}%
+	</td>`;
+        };
+
+        if (bp >= 26 && bp < 50) {
+            return `
+	<td class="battery">
+	<i class="fas fa-battery-half"></i> ${bp}%
+	</td>`;
+        };
+
+        if (bp >= 51 && bp < 75) {
+            return `
+	<td class="battery">
+	<i class="fas fa-battery-three-quarters"></i> ${bp}%
+	</td>`;
+        };
+
+        if (bp >= 76 && bp <= 100) {
+            return `
+	<td class="battery">
+	<i class="fas fa-battery-full"></i> ${bp}%
+	</td>`;
+        };
+
+    },
+
+    socketNotificationReceived(notification, payload) {
+        const self = this;
+
+        switch (notification) {
+            case 'STATS':
+                self.loaded = true;
+                self.stats = payload;
+                break;
+            case 'ERROR':
+                self.error = payload;
+                break;
+        }
+
+        this.updateDom(self.config.animationSpeed);
+    },
+
+    getScripts() {
+        return [];
+    },
+
+    getStyles() {
+        return [
+            'MMM-XiaomiVacuum.css',
+            'font-awesome.css',
+        ];
+    },
+
+    getTranslations() {
+        return {
+            en: 'translations/en.json'
+        };
+    },
+});
---
+++ node_helper.js
@@ -0,0 +1,135 @@
+/* Magic Mirror
+ * Node Helper: MMM-XiaomiVacuum
+ *
+ * By John Kristensen
+ * MIT Licensed.
+ */
+
+
+const NodeHelper = require('node_helper');
+const miio = require('./miio');
+
+const REQUIRED_FIELDS = ['token', 'ipAddress'];
+
+module.exports = NodeHelper.create({
+
+    // Start
+    start: function() {
+        const self = this;
+
+        self.started = false;
+        self.config = [];
+        self.stats = {};
+    },
+
+    // socket Notification Received
+    socketNotificationReceived: function(notification, payload) {
+        const self = this;
+
+        switch (notification) {
+            case 'START':
+                self.handleStartNotification(payload);
+        }
+    },
+
+    // handle Start Notification
+    handleStartNotification: function(payload) {
+        const self = this;
+
+        if (self.started) {
+            return;
+        }
+
+        self.config = payload;
+
+        if (self.isInvalidConfig()) {
+            return;
+        }
+
+        self.scheduleUpdates();
+
+        self.started = true;
+    },
+
+    /*
+    Connected to MiioDevice {
+      model=roborock.vacuum.s5,
+      types=miio:vacuum, miio, vaccuum,
+      capabilities=adjustable-fan-speed, fan-speed, spot-cleaning, autonomous-cleaning,
+      cleaning-state, error-state, charging-state, battery-level, state
+    }
+    */
+
+    // *** Update Stats ***
+    updateStats: function() {
+        const self = this;
+
+
+        // Connect to the vacuum cleaner and get various status
+        var device = miio.device({
+                address: self.config.ipAddress,
+                token: self.config.token,
+            })
+            .then(device => {
+
+                // console.log('Connected to', device.model()); // VIRKER !!
+                // console.log('Model         : ' + device.miioModel);
+                // console.log('State         : ' + device.property('state'));
+                // console.log('Battery Level : ' + device.property('batteryLevel'));
+                //console.log('Fan Speed     : ' + device.property('fanSpeed'));
+                //console.log('mainBrush Work Time : ' + device.property('mainBrushWorkTime'));
+                //console.log('sideBrush Work Time : ' + device.property('sideBrushWorkTime'));
+                //console.log('filter Work Time : ' + device.property('filterWorkTime'));
+                //console.log('sensor Dirty Time : ' + device.property('sensorDirtyTime'));
+
+                Object.assign(self.stats, {
+                    name: device.miioModel.toUpperCase(),
+                    batteryPercent: device.property('batteryLevel'),
+                    phase: device.property('state'),
+                    fanspeed: device.property('fanSpeed'),
+                    filterworktime: device.property('filterWorkTime'),
+                    sidebrush: device.property('sideBrushWorkTime'),
+                    mainBrush: device.property('mainBrushWorkTime'),
+                    sensordirtytime: device.property('sensorDirtyTime')
+                });
+
+                device.destroy();
+
+                self.sendSocketNotification('STATS', self.stats);
+            })
+            .catch(err => console.log('Connection failure'));
+
+        return
+
+    },
+
+
+    // Validate config, and report back if fields are missing
+    isInvalidConfig: function() {
+        const self = this;
+
+        let missingField = REQUIRED_FIELDS.find((field) => {
+            return !self.config[field];
+        });
+
+        if (missingField) {
+            self.sendSocketNotification(
+                'ERROR',
+                `<i>Confg.${missingField}</i> is required for module: ${self.name}.`
+            );
+        }
+        return !!missingField;
+    },
+
+    // schedule Updates
+    scheduleUpdates() {
+        const self = this;
+
+        self.updateStats();
+        setInterval(function() {
+            self.updateStats();
+        }, self.config.updateInterval);
+    },
+
+
+});
